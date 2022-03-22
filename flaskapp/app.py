201492928e1c77ac6ace61e2a05b2c7678f20e33
Abiram <abiram.sudhakar18@siesgst.ac.in>
2022-03-22 12:04:14 +0530

    fl

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template("landing.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
